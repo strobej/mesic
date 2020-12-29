fcc9bd2e8f4a29801f650d3499785670dc58f012
Selma <selma.delic@outlook.com>
2020-12-29 15:49:22 +0100

    mobile desing

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,13 +17,12 @@ const [getContent, setContent] = useState("");
         <button id="service-btn" onClick={() => {setTitle("commercial"); setContent("commercialtext"); setModalIsOpen(true)}}>{data[props.language].commercial}</button>
         <button id="service-btn" onClick={() => {setTitle("enforcement"); setContent("enforcementtext"); setModalIsOpen(true)}}>{data[props.language].enforcement}</button>
         <button id="service-btn" onClick={() => {setTitle("labor"); setContent("labortext"); setModalIsOpen(true)}}>{data[props.language].labor}</button>
-        <button id="service-btn" onClick={() => {setTitle("status"); setContent("statustext"); setModalIsOpen(true)}}>{data[props.language].status}</button>
         <button id="service-btn" onClick={() => {setTitle("family"); setContent("familytext"); setModalIsOpen(true)}}>{data[props.language].family}</button>
         <button id="service-btn" onClick={() => {setTitle("international"); setContent("internationaltext"); setModalIsOpen(true)}}>{data[props.language].international}</button>
     
         <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="mymodal">
             <h2>{data[props.language][getTitle]}</h2>
-            <p>{data[props.language][getContent]}</p>
+            <p className="text-modal">{data[props.language][getContent]}</p>
 
             <div>
               <button className="close-btn" onClick={() => {setTitle("close"); setModalIsOpen(false)}}>{data[props.language].close}</button>
